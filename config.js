73b303def909c8ddb23f889bd3bd02a84d0735ca
saurabhxrolex <rolexdlr53@gmail.com>
2026-07-17 00:09:53 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  OWNER: "918601322748@s.whatsapp.net",
+
+  PREFIX: ".",
+
+  WARNING_LIMIT: 3,
+
+  ANTILINK: true,
+
+  ANTIBADWORD: true,
+
+  AUTO_DELETE: true,
+
+  AUTO_KICK: true,
+
+  IGNORE_ADMINS: true,
+
+  IGNORE_OWNER: true
+};
